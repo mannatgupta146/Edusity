9f23b1a65a792f809a052a2813834c5ef8342b38
mannatgupta146 <mannatgupta146@gmail.com>
2025-08-27 01:46:05 +0530

    added

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './VideoPlayer.css'
 import collegeVideo from '../../assets/college-video.mp4'
 
 const VideoPlayer = ({play, setPlay}) => {
+
+    const player = useRef(null);
+
+    const closePlayer = (e) => {
+        if(e.target === player.current){
+            setPlay(false);
+        }
+    }
+
   return (
-    <div className={`video-player ${play ? '' : 'hide'}`}>
+    <div className={`video-player ${play ? '' : 'hide'}`} ref={player} onClick={closePlayer}>
       <video src={collegeVideo} autoplay muted controls></video>
     </div>
   )
