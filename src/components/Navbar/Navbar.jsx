f17430aa406dd60bb6461e7fd59929ced3bef058
mannatgupta146 <mannatgupta146@gmail.com>
2025-08-26 22:21:52 +0530

    added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,11 +11,16 @@ const Navbar = () => {
         window.scrollY > 50 ? setSticky(true) : setSticky(false)
     })  
   },[])
+  
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu=()=>{
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
 
   return (
     <nav className={`container navbar ${sticky ? "dark-nav" : ""}`}>
         <img src={logo} alt="" className="logo" />
-        <ul>
+        <ul className={mobileMenu? '' : 'mobile-menu'}>
             <li><Link to="hero" smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to="program" smooth={true} offset={-260} duration={500}>Program</Link></li>
             <li><Link to="about" smooth={true} offset={-165} duration={500}>About us</Link></li>
@@ -23,7 +28,7 @@ const Navbar = () => {
             <li><Link to="testimonials" smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
             <li><Link to="contact" smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
         </ul>
-        <img src={menuIcon} alt="" className='menu-icon'/>
+        <img src={menuIcon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
