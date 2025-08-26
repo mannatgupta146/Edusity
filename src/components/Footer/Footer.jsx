0d999af929196a7bf4927354abc4e257fe3455d8
mannatgupta146 <mannatgupta146@gmail.com>
2025-08-26 15:07:22 +0530

    added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,12 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='footer'>
+      <p>&copy; 2025 Edusity. All rights reserved. </p>
+        <ul>
+          <li>Terms of Services</li>
+          <li>Privacy Policy</li>
+        </ul>
     </div>
   )
 }
